a2ca1abab37b55e6657f5d9c615c8af818ce21f7
admin <elena.rabano.perez@iesjulianmarias.es>
2024-11-22 15:28:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/examen1andres/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.examen1andres
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var iVCyltv : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        iVCyltv = findViewById(R.id.iVCyltv)
+
+        iVCyltv.setOnClickListener {
+
+
+
+        }
+
+    }
+}
